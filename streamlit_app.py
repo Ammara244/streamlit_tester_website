5580e9bef391c3bf4b79e27382dd80e9c3592eac
Ammara244 <ammarahajat24@gmail.com>
2025-07-31 20:12:57 +0000

    attempted_timedelay_thing

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ st.title("🎈 My new Strealmit app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+import time 
+def num_generator():
+    input('Whats your name? ')
+    for i in range(3):
+        yield i
+        time.sleep(2)
+
+st.write_stream(num_generator)
